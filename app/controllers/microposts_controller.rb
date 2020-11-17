d00ebff8d487ad506956df658ba769a8dc488488
nick22nd <yxdgg1119@gmail.com>
2020-11-17 11:27:50 +0800

    Add micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,27 @@
 class MicropostsController < ApplicationController
+    before_action :logged_in_user , only: [:create, :destroy]
+    def create
+        @micropost = current_user.microposts.build(micropost_params)
+        if @micropost.save
+            flash[:success] = "Micropost created!"
+            redirect_to root_url
+        else
+            render 'static_pages/home'
+        end
+    end
+
+    def destroy
+        # @micropost = Micropost.find(params[:id])
+        # if @micropost.destroy
+        #     flash[:success] = 'Object was successfully deleted.'
+        #     redirect_to microposts_url
+        # else
+        #     flash[:error] = 'Something went wrong'
+        #     redirect_to microposts_url
+        # end
+    end
+    private
+        def micropost_params
+            params.require(:micropost).permit(:content)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :edit, :create, :update]
+  resources :microposts, only: [:create, :destroy]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
